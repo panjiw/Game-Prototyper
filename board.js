1788f87d3f51b2d8d11e9d7891a2ea228bb1084d
Panji Wisesa <panji.wisesa@outlook.com>
2016-03-09 17:10:50 -0800

    add board holding actors and tiles

--- board.js
+++ board.js
@@ -8,6 +8,10 @@
  *  - tile: the tile on that position
  */
 
+if (typeof(module) !== 'undefined') {
+  var Actor = require('./actor.js').Actor;
+}
+
 (function() {
   var Board = (function() {
     var Board = function(h, w) {
@@ -15,21 +19,59 @@
       this.w = w;
       this.actors = [];
       this.tiles = [];
-      for (var i = 0; i < h; i++) {
+      for (var i = 0; i < w; i++) {
         this.actors[i] = [];
         this.tiles[i] = [];
-        for (var j = 0; j < w; j++) {
+        for (var j = 0; j < h; j++) {
           this.actors[i][j] = [];
+        }
+      }
+      this.game = undefined;
+    };
+
+    Board.prototype.fillTiles = function(symbol2D) {
+      for (var x = 0; x < this.w; x++) {
+        for (var y = 0; y < this.h; y++) {
+          var symbol = symbol2D[this.h - 1 - y][x];
+          this.tiles[x][y] = Actor.symbolMap[symbol].clone();
+          this.tiles[x][y].x = x;
+          this.tiles[x][y].y = y;
         }
       }
     };
 
-    Board.prototype.fillTiles = function fillTiles(jsonMap) {
-
+    Board.prototype.fillActors = function(symbol3D) {
+      for (var x = 0; x < this.h; x++) {
+        for (var y = 0; y < this.w; y++) {
+          var symbols = symbol3D[this.h - 1 - y][x];
+          for (var symbol in symbols) {
+            this.moveActor(Actor.symbolMap[symbol].clone(), x, y);
+          }
+        }
+      }
     };
 
-    Board.prototype.fillActors = function fillActors(jsonMap) {
+    Board.prototype.moveActor = function(actor, x, y) {
+      if (actor.x != -1 && actor.y != -1) {
+        var i = this.actors[actor.x][actor.y].indexOf(actor);
+        if (i > -1) {
+          this.actors[actor.x][actor.y] = this.actors[actor.x][actor.y].splice(i, 1);
+        }
+      }
 
+      this.actors[x][y].push(actor);
+      actor.x = x;
+      actor.y = y;
+    };
+
+    Board.prototype.setActive = function(requirement) {
+      for (var x = 0; x < this.h; x++) {
+        for (var y = 0; y < this.w; y++) {
+          for (var actor in this.actors[x][y]) {
+            actor.active = actor.passRequirement(requirement);
+          }
+        }
+      }
     };
 
     return Board;
