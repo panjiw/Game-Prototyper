d45a5eb0131969aa7e530e8c46fecedfa6700d49
Panji Wisesa <panji.wisesa@outlook.com>
2016-03-09 17:11:55 -0800

    add game holding the board and the turns

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 (function() {
   var Game = (function() {
-    var Game = function() {
-      this.actors = [];
-      this.board = undefined;
-      this.turns = undefined;
+    var Game = function(board, turns) {
+      this.board = board;
+      this.turns = turns;
+      this.board.game = this;
+      this.turns.game = this;
     };
 
     return Game;
