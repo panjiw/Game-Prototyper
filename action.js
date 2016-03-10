a9ce732fbccded15314ec7c7416f8317539049c1
Panji Wisesa <panji.wisesa@outlook.com>
2016-03-09 17:11:10 -0800

    add action for actors

--- action.js
+++ action.js
@@ -3,7 +3,7 @@
  * Properties:
  *  - name: name of the action
  *  - description: description of the action
- *  - checker: a function that takes 2 actors (self, other) and returns a bool
+ *  - applicable: a function that takes 2 actors (self, other) and returns a bool
  *             based on whether this action can be done by self on other
  *  - act: a function that takes 2 actors (self, other) and returns a nothing
  *         but represents in code what this action does
@@ -14,12 +14,12 @@
     var Action = function(name, description) {
       this.name = name;
       this.description = description;
-      this.checker = function(self, other) { return false; };
+      this.applicable = function(self, other) { return false; };
       this.act = function(self, other) {};
     };
 
-    Action.prototype.setChecker = function(checker) {
-      this.checker = checker;
+    Action.prototype.setApplicabilityChecker = function(checker) {
+      this.applicable = checker;
     };
 
     Action.prototype.setAct = function(act) {
